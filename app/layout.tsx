17f03e3640c2bb927912797f0a2d4f8245eb8c68
Sahal-Palayat <sahalpalayat5894@gmail.com>
2025-04-15 22:44:12 +0530

    sa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,12 +15,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/Products/Pebby-icon.svg" />
+        <link rel="icon" href="/Products/Pebby-icon.svg" />
+        
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-QG9RR3Q1N2"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QG9RR3Q1N2');
+            `,
+          }}
+        />
       </head>
-      <body
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
