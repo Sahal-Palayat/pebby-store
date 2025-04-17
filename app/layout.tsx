482798675b061db8c622f526476fbd4b734cb36e
Sahal-Palayat <sahalpalayat5894@gmail.com>
2025-04-17 22:19:41 +0530

    chnge icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/Products/Pebby-icon.svg" />
+        <link rel="icon" href="/PebbyLogo.png"/>
 
         {/* Google Analytics Script */}
         <Script
